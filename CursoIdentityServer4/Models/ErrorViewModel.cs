c8695ef27f4c7acf0a5ac4caf79cae9200d917af
Jean Michael <jeanmmclaudino@gmail.com>
2022-05-29 20:21:06 -0300

    Add project files.

---
+++ CursoIdentityServer4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CursoIdentityServer4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
